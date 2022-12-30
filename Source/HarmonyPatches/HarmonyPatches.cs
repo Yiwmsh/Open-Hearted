9ff7e04d93df0afa8eb7f88bcada76b4817320c5
yiwmsh <lonelevelsands@gmail.com>
2022-12-30 16:54:02 -0700

    🚧

--- Source/HarmonyPatches/HarmonyPatches.cs
+++ Source/HarmonyPatches/HarmonyPatches.cs
@@ -1,16 +1,42 @@
 ﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
 using System.Reflection;
 using Verse;
+using Verse.AI;
 
 namespace Open_Hearted
 {
 	[StaticConstructorOnStartup]
-	public static class Main
+	public static class HarmonyPatches
 	{
-		static Main()
+		private static readonly Type patchType = typeof(HarmonyPatches);
+
+		static HarmonyPatches()
 		{
-			var harmony = new Harmony("com.openhearted");
-			harmony.PatchAll(Assembly.GetExecutingAssembly());
+			Harmony harmony = new Harmony("com.openhearted");
+
+			Harmony.DEBUG = true;
+
+			FileLog.Log("Open Hearted");
+
+			var assembly = Assembly.GetExecutingAssembly();
+
+			harmony.PatchAll(assembly);
+		}
+	}
+
+	[HarmonyPatch(typeof(MentalBreaker))]
+	[HarmonyPatch("CurrentPossibleMoodBreaks", MethodType.Getter)]
+	public static class CurrentPossibleMoodBreaks_OpenHearted
+	{
+		[HarmonyPrefix]
+		public static bool CurrentPossibleMoodBreaks_Prefix(ref IEnumerable<MentalBreakDef> __result, MentalBreaker __instance)
+		{
+			//Need to make a delegate to get MentalBreaker.pawn because it's private.
+
+			//If __instance.pawn has gene Open_Hearted set __result to ["GriefDeath"] or something like that and return false
+			//Else return true
 		}
 	}
 }
