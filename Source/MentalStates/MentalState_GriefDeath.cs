db9696d7b49e40284c84a71a71639b8c449bcde0
yiwmsh <lonelevelsands@gmail.com>
2023-01-01 15:58:57 -0700

    🧼 And slightly cleaner still

--- Source/MentalStates/MentalState_GriefDeath.cs
+++ Source/MentalStates/MentalState_GriefDeath.cs
@@ -3,9 +3,10 @@ namespace Open_Hearted
 {
 	public class MentalState_GriefDeath : MentalState
 	{
-		public override void PreStart()
+
+		public override void MentalStateTick()
 		{
-			base.PreStart();
+			base.MentalStateTick();
 			base.pawn.Kill(null, null);
 		}
 	}
